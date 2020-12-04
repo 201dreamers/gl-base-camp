b536beaae3cc5c138edea25e60728268e54f602f
201dreamers <201dreamers@gmail.com>
2020-12-04 22:27:18 +0200

    Created framework, done homework 9

---
+++ framework-hw9/api/reqres_api_client.py
@@ -0,0 +1,56 @@
+from os import stat
+import requests
+from urllib.parse import urljoin
+
+from endpoints.endpoints import Endpoints
+from config.config import Config
+
+
+class ReqResApiClient():
+
+    __slots__ = ()
+
+    @staticmethod
+    def _prepare_url(url, base_url=Config.REQRES_BASE_URL):
+        return urljoin(base_url, url)
+
+    @staticmethod
+    def list_users():
+        response = requests.get(
+            ReqResApiClient._prepare_url(Endpoints.USERS_URL))
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def create_user(user_details):
+        response = requests.post(
+            ReqResApiClient._prepare_url(Endpoints.USERS_URL),
+            json=user_details
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def register(user_credentials):
+        response = requests.post(
+            ReqResApiClient._prepare_url(Endpoints.REGISTER_URL),
+            json=user_credentials
+        )
+        response.raise_for_status()
+        return response.json() 
+
+    @staticmethod
+    def login(user_credentials):
+        response = requests.post(
+            ReqResApiClient._prepare_url(Endpoints.LOGIN_URL),
+            json=user_credentials
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def list_unknown():
+        response = requests.get(
+            ReqResApiClient._prepare_url(Endpoints.UNKNOWN_URL))
+        response.raise_for_status()
+        return response.json()
---
+++ framework-hw9/endpoints/endpoints.py
@@ -0,0 +1,7 @@
+class Endpoints:
+
+    API_URL = '/api'
+    USERS_URL = f'{API_URL}/users'
+    REGISTER_URL = f'{API_URL}/register'
+    LOGIN_URL = f'{API_URL}/login'
+    UNKNOWN_URL = f'{API_URL}/unknown'
